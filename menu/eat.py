a8186a7d308c6f1a727a9bfb849e857c00adc29a
Mikhail Olenin <ufian@yandex-team.ru>
2016-07-09 14:44:11 +0300

    Use base eat menu

--- menu/eat.py
+++ menu/eat.py
@@ -76,6 +76,8 @@ class BaseEatMenu(BaseMenu):
         if num is None or num <= 0:
             return None, u'Неправильное число'
 
+        del data['text']
+
         return num, None
 
     STEPS = []
@@ -120,7 +122,6 @@ class EatCMenu(BaseEatMenu):
 
     STEPS = [
         ('calories', BaseEatMenu.q_full_calories, BaseEatMenu.getter_int),
-        ('weight', BaseEatMenu.q_weight, BaseEatMenu.getter_int)
     ]
 
 class EatCWMenu(BaseEatMenu):
@@ -130,131 +131,3 @@ class EatCWMenu(BaseEatMenu):
         ('calories', BaseEatMenu.q_calories_by_weight, BaseEatMenu.getter_int),
         ('weight', BaseEatMenu.q_weight, BaseEatMenu.getter_int)
     ]
-
-
-'''class EatCMenu(BaseMenu):
-    TYPE = 'EatC'
-
-
-
-    SAVE_BUTTON = (u'Сохранить', 'save')
-
-
-    @classmethod
-    def save(self, context):
-        row = context.params
-
-        context.ccalories.add_eating(
-            user_id=context.user_id,
-            product_id=None,
-            calories=row.get('calories'),
-            weight=row.get('weight')
-        )
-
-    @classmethod
-    def get(cls, context, data):
-        if 'cb_data' in data and 'from' not in data:
-            if data['cb_data'] in ('save', 'back'):
-                context.set('Main')
-                return None
-
-        if context.params is None:
-            context.params = dict()
-            return {
-                'text': u'Введите общую калорийность съеденого',
-                'buttons': [[
-                    cls.BACK_BUTTON
-                ]]
-            }
-
-        elif 'text' in data:
-            if 'calories' not in context.params:
-                calories = u.get_int(data['text'])
-                if calories is None or calories <= 0:
-                    return {
-                        'text': u'Неправильное число. Введите общую калорийность съеденого',
-                        'buttons': [[
-                            cls.BACK_BUTTON,
-                        ]]
-                    }
-                context.params['calories'] = calories
-                cls.save(context)
-                data['from_message'] = u'Сохранено'
-                context.set('Main')
-                return None
-
-        data['from_message'] = u'Ошибка'
-        context.set('Main')
-        return None
-
-
-
-class EatCWMenu(BaseMenu):
-    TYPE = 'EatCW'
-
-    SAVE_BUTTON = (u'Сохранить', 'save')
-
-
-    @classmethod
-    def save(self, context):
-        row = context.params
-
-        context.ccalories.add_eating(
-            user_id=context.user_id,
-            product_id=None,
-            calories=row.get('calories'),
-            weight=row.get('weight')
-        )
-
-    @classmethod
-    def get(cls, context, data):
-        if 'cb_data' in data and 'from' not in data:
-            if data['cb_data'] in ('save', 'back'):
-                context.set('Main')
-                return None
-
-        if context.params is None:
-            context.params = dict()
-            return {
-                'text': u'Введите калорийность за 100 грамм',
-                'buttons': [[
-                    cls.BACK_BUTTON
-                ]]
-            }
-        elif 'text' in data:
-            if 'calories' not in context.params:
-                calories = u.get_int(data['text'])
-                if calories is None or calories <= 0:
-                    return {
-                        'text': u'Неправильное число. Введите калорийность за 100 грамм',
-                        'buttons': [[
-                            cls.BACK_BUTTON,
-                        ]]
-                    }
-                context.params['calories'] = calories
-
-                return {
-                    'text': u'Введите вес в граммах',
-                    'buttons': [[
-                        cls.BACK_BUTTON,
-                    ]]
-                }
-            elif 'weight' not in context.params:
-                weight = u.get_int(data['text'])
-                if weight is None or weight <= 0:
-                    return {
-                        'text': u'Неправильное число. Введите вес в граммах',
-                        'buttons': [[
-                            cls.BACK_BUTTON,
-                        ]]
-                    }
-
-                context.params['weight'] = weight
-                cls.save(context)
-                data['from_message'] = u'Сохранено'
-                context.set('Main')
-                return None
-
-        data['from_message'] = u'Ошибка'
-        context.set('Main')
-        return None'''
